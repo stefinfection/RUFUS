b5e47e72a89f1193cc220cc073d2759cb24352ab
stefinfection <sjeangeo@gmail.com>
2024-01-08 19:11:25 +0000

    Another absolute path in test script

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -6,4 +6,4 @@ else
 	exit
 fi 
 
-/home/ubuntu/RUFUS/runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa $1
+/home/ubuntu/RUFUS/runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r /home/ubuntu/RUFUS/resources/references/small_test_human_reference_v37_decoys.fa $1
