9394c354f777c62786a742e8bcbec0d36cba64d4
stefinfection <sjeangeo@gmail.com>
2024-01-08 19:18:54 +0000

    pwd also does not work in script

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -6,4 +6,6 @@ else
 	exit
 fi 
 
-/home/ubuntu/RUFUS/runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r /home/ubuntu/RUFUS/resources/references/small_test_human_reference_v37_decoys.fa $1
+RUFUS_PATH=/home/ubuntu/RUFUS
+RUFUS_TEST_PATH="${RUFUS_PATH}/testRun"
+${RUFUS_PATH}/runRufus.sh -s ${RUFUS_TEST_PATH}/Child.bam -c ${RUFUS_TEST_PATH}/Mother.bam -c ${RUFUS_TEST_PATH}/Father.bam -k 25 -t 40  -r ${RUFUS_PATH}/resources/references/small_test_human_reference_v37_decoys.fa $1
