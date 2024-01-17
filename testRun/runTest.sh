140b9d8747c2b253ed466358c6682b82f29ae6c6
stefinfection <sjeangeo@gmail.co>
2024-01-16 23:26:30 -0700

    Update to opt path

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -1,11 +1,11 @@
-if [ -e /home/ubuntu/RUFUS/runRufus.sh ]
+if [ -e /opt/RUFUS/runRufus.sh ]
 then 
 	echo "starting test"
 else
-	echo "could not find ./../runRufus.sh, are you in the RUFUS/runTest/ directory?"
+	echo "could not find /opt/runRufus.sh, are you in the RUFUS/runTest/ directory?"
 	exit
 fi 
 
-RUFUS_PATH=/home/ubuntu/RUFUS
+RUFUS_PATH=/opt/RUFUS
 RUFUS_TEST_PATH="${RUFUS_PATH}/testRun"
 ${RUFUS_PATH}/runRufus.sh -s ${RUFUS_TEST_PATH}/Child.bam -c ${RUFUS_TEST_PATH}/Mother.bam -c ${RUFUS_TEST_PATH}/Father.bam -k 25 -t 40  -r ${RUFUS_PATH}/resources/references/small_test_human_reference_v37_decoys.fa $1
