4f511cfbda874616706a7fe6e43911970c4ce52c
stefinfection <sjeangeo@gmail.co>
2024-01-16 23:41:29 -0700

    Update to accommodate mnt dir for test

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -7,5 +7,5 @@ else
 fi 
 
 RUFUS_PATH=/opt/RUFUS
-RUFUS_TEST_PATH="${RUFUS_PATH}/testRun"
+RUFUS_TEST_PATH=/mnt
 ${RUFUS_PATH}/runRufus.sh -s ${RUFUS_TEST_PATH}/Child.bam -c ${RUFUS_TEST_PATH}/Mother.bam -c ${RUFUS_TEST_PATH}/Father.bam -k 25 -t 40  -r ${RUFUS_PATH}/resources/references/small_test_human_reference_v37_decoys.fa $1
