fbcee41191993d731be72c270b95403fed27ae96
stefinfection <sjeangeo@gmail.co>
2024-03-06 11:43:52 -0700

    Fix merge conflict build_rufus script and cleanup rufus.def a bit

--- singularity/build_rufus.sh
+++ singularity/build_rufus.sh
@@ -1,7 +1,3 @@
 export SINGULARITY_TMPDIR=/home/ubuntu/singularity_tmp
 export SINGULARITY_BIND=/home/ubuntu/rufus_data:/mnt
-<<<<<<< HEAD
 sudo -E singularity build --sandbox rufus.sif rufus.def
-=======
-sudo -E singularity build rufus.sif rufus.def
->>>>>>> 927c60278967e8c3940f191d74f3b7fe96aefa0c
