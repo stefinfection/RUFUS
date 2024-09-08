0ba348b7c4b3eb3b0ec4a2f382fc2af37cc7c434
stefinfection <sjeangeo@gmail.com>
2024-09-08 19:14:28 +0000

    refactor to throttle if windows exceed array limit

--- singularity/setup_slurm.sh
+++ singularity/setup_slurm.sh
@@ -41,71 +41,96 @@ fi
 
 # Compose run script(s)
 echo -en "##RUFUS_callCommand=" > rufus.cmd
-	
+
+# Figure out how many call scripts we need
 NUM_CALL_SLURMS=$(( NUM_CHUNKS / SLURM_ARRAY_JOB_LIMIT_RUFUS_ARG ))
 REM=$(( NUM_CHUNKS % SLURM_ARRAY_JOB_LIMIT_RUFUS_ARG ))
 if [ "$REM" -gt 0 ]; then
-	NUM_CALL_SLURMS=$(( NUM_CALL_SLURMS + 1 ))
+    NUM_CALL_SLURMS=$(( NUM_CALL_SLURMS + 1 ))
 fi
 
-i=1
-while [ "$i" -le "$NUM_CALL_SLURMS" ]; do
-    CURR_CALL_SCRIPT="rufus_call_${i}.slurm"
-	for line in "${HEADER_LINES[@]}"
-	do
-    	echo -e "$line" >> $CURR_CALL_SCRIPT
-	done
+# If doing non-windowed run, or fits into array, similar composure
+SINGLE_MODE="FALSE"
+CALL_SLURM_SCRIPT="rufus_scaffold.slurm"
+if [ "$WINDOW_SIZE_RUFUS_ARG" = "0" ] || [ "$NUM_CALL_SLURMS" = "1" ]; then
+	SINGLE_MODE="TRUE"
+	CALL_SLURM_SCRIPT="rufus_call.slurm"
+fi	
 
-	if [ ! -z $EMAIL_RUFUS_ARG ]; then
-    	echo -e "#SBATCH --mail-type=ALL" >> $CURR_CALL_SCRIPT
-    	echo -e "#SBATCH --mail-user=${EMAIL_RUFUS_ARG}" >> $CURR_CALL_SCRIPT
-	fi		
+# Compose the call script
+for line in "${HEADER_LINES[@]}"
+do
+   	echo -e "$line" >> $CALL_SLURM_SCRIPT
+done
 
-	if [ "$WINDOW_SIZE_RUFUS_ARG" = "0" ]; then
-    	echo "" >> $CURR_CALL_SCRIPT
-    	echo -e "REGION_ARG=\"\"" >> $CURR_CALL_SCRIPT
+if ["$SINGLE_MODE" = "FALSE" ];
+	echo -e "REGION_INDEX=\$1" >> $CALL_SLURM_SCRIPT
+fi
+
+if [ ! -z $EMAIL_RUFUS_ARG ]; then
+   	echo -e "#SBATCH --mail-type=ALL" >> $CALL_SLURM_SCRIPT
+   	echo -e "#SBATCH --mail-user=${EMAIL_RUFUS_ARG}" >> $CALL_SLURM_SCRIPT
+fi		
+
+if [ "$WINDOW_SIZE_RUFUS_ARG" = "0" ]; then
+   	echo "" >> $CALL_SLURM_SCRIPT
+   	echo -e "REGION_ARG=\"\"" >> $CALL_SLURM_SCRIPT
+else
+	ADJUSTED_END=$(( SLURM_ARRAY_JOB_LIMIT_RUFUS_ARG - 1 ))
+	
+	# Only use arrays if we fit into a single script
+	if [ "$SINGLE_MODE" = "TRUE" ]; then
+   		echo -e "#SBATCH -a 0-${ADJUSTED_END}%${SLURM_JOB_LIMIT_RUFUS_ARG}" >> $CALL_SLURM_SCRIPT
+   		echo "" >> $CURR_CALL_SCRIPT
+   		echo -e "region_arg=\$(singularity exec ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/singularity/launch_utilities/get_region.sh \"\$SLURM_ARRAY_TASK_ID\" \"$WINDOW_SIZE_RUFUS_ARG\" \"$GENOME_BUILD_RUFUS_ARG\")" >> $CURR_CALL_SCRIPT
 	else
-		ADJ_INDEX=$(( i - 1 ))
-		CURR_ARR_START=$(( ADJ_INDEX * SLURM_ARRAY_JOB_LIMIT_RUFUS_ARG ))
-		CURR_ARR_END=$(( i * SLURM_ARRAY_JOB_LIMIT_RUFUS_ARG - 1 ))
-		
-		# If on the last script, adjust to the remainder
-		if [ "$i" = "$NUM_CALL_SLURMS" ]; then
-			CURR_ARR_END=$(( CURR_ARR_START + REM - 1 ))
-		fi
-
-    	echo -e "#SBATCH -a ${CURR_ARR_START}-${CURR_ARR_END}%${SLURM_JOB_LIMIT_RUFUS_ARG}" >> $CURR_CALL_SCRIPT
-    	echo "" >> $CURR_CALL_SCRIPT
-    	echo -e "region_arg=\$(singularity exec ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/singularity/launch_utilities/get_region.sh \"\$SLURM_ARRAY_TASK_ID\" \"$WINDOW_SIZE_RUFUS_ARG\" \"$GENOME_BUILD_RUFUS_ARG\")" >> $CURR_CALL_SCRIPT
-    	echo -e "REGION_ARG=\"-R \$region_arg\"" >> $CURR_CALL_SCRIPT
+		echo "" >> $CURR_CALL_SCRIPT
+        echo -e "region_arg=\$(singularity exec ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/singularity/launch_utilities/get_region.sh \"\$REGION_INDEX\" \"$WINDOW_SIZE_RUFUS_ARG\" \"$GENOME_BUILD_RUFUS_ARG\")" >> $CURR_CALL_SCRIPT
 	fi
 
-	echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> $CURR_CALL_SCRIPT
-	echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> rufus.cmd
+   	echo -e "REGION_ARG=\"-R \$region_arg\"" >> $CALL_SLURM_SCRIPT
+fi
 
-	# Add control args
-	for control in "${CONTROLS_RUFUS_ARG[@]}"; do
-   		echo -en "-c $control " >> $CURR_CALL_SCRIPT
-   		echo -en "-c $control " >> rufus.cmd
-	done	
+echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> $CALL_SLURM_SCRIPT
+echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> rufus.cmd
 
-	# Add in optional hashes if provided
-	if [ ! -z "$REFERENCE_HASH_RUFUS_ARG" ]; then
-   		echo -en "-f $REFERENCE_HASH_RUFUS_ARG " >> $CURR_CALL_SCRIPT
-   		echo -en "-f $REFERENCE_HASH_RUFUS_ARG " >> rufus.cmd
-	fi
-	if [ ! -z "$EXCLUDE_HASH_LIST_RUFUS_ARG" ]; then
-   		for exclude in "${EXCLUDE_HASH_LIST_RUFUS_ARG[@]}"; do
-       		echo -en "-e $exclude " >> $CURR_CALL_SCRIPT
-       		echo -en "-e $exclude " >> rufus.cmd
-   		done
-	fi
-
-	echo -e "-r $REFERENCE_RUFUS_ARG -m $KMER_DEPTH_CUTOFF_RUFUS_ARG -k 25 -t $THREAD_LIMIT_RUFUS_ARG -L -vs \$REGION_ARG" >> $CURR_CALL_SCRIPT
-	echo -e "-r $REFERENCE_RUFUS_ARG -m $KMER_DEPTH_CUTOFF_RUFUS_ARG -k 25 -t $THREAD_LIMIT_RUFUS_ARG -L -vs \$REGION_ARG" >> rufus.cmd
-
-   	i=$(( i + 1 ))
+# Add control args
+for control in "${CONTROLS_RUFUS_ARG[@]}"; do
+	echo -en "-c $control " >> $CALL_SLURM_SCRIPT
+   	echo -en "-c $control " >> rufus.cmd
 done	
+
+# Add in optional hashes if provided
+if [ ! -z "$REFERENCE_HASH_RUFUS_ARG" ]; then
+  	echo -en "-f $REFERENCE_HASH_RUFUS_ARG " >> $CALL_SLURM_SCRIPT
+   	echo -en "-f $REFERENCE_HASH_RUFUS_ARG " >> rufus.cmd
+fi
+if [ ! -z "$EXCLUDE_HASH_LIST_RUFUS_ARG" ]; then
+   	for exclude in "${EXCLUDE_HASH_LIST_RUFUS_ARG[@]}"; do
+    	echo -en "-e $exclude " >> $CALL_SLURM_SCRIPT
+       	echo -en "-e $exclude " >> rufus.cmd
+   	done
+fi
+
+echo -e "-r $REFERENCE_RUFUS_ARG -m $KMER_DEPTH_CUTOFF_RUFUS_ARG -k 25 -t $THREAD_LIMIT_RUFUS_ARG -L -vs \$REGION_ARG" >> $CALL_SLURM_SCRIPT
+echo -e "-r $REFERENCE_RUFUS_ARG -m $KMER_DEPTH_CUTOFF_RUFUS_ARG -k 25 -t $THREAD_LIMIT_RUFUS_ARG -L -vs \$REGION_ARG" >> rufus.cmd
+
+# Make throttled slurm script to run scaffold	
+if [ "$SINGLE_MODE" = "FALSE" ];
+	CALL_SLURM_SCRIPT="rufus_call.slurm"
+	SCAFFOLD_SLURM_SCRIPT="rufus_scaffold.slurm"
+    echo -e "max_concurrent_jobs=$SLURM_JOB_LIMIT_RUFUS_ARG" >> $CALL_SLURM_SCRIPT
+    echo -e "for ((i=0; i<$NUM_CHUNKS; i+=$SLURM_JOB_LIMIT_RUFUS_ARG)); do" >> $CALL_SLURM_SCRIPT
+    echo -e "   running_jobs=$(squeue -u $USER | wc -l)" >> $CALL_SLURM_SCRIPT
+    echo -e "   while [ "$running_jobs" -ge "$max_concurrent_jobs" ]; do" >> $CALL_SLURM_SCRIPT
+    echo -e "       sleep 5m" >> $CALL_SLURM_SCRIPT
+    echo -e "       running_jobs=$(squeue -u $USER | wc -l)" >> $CALL_SLURM_SCRIPT
+    echo -e "   done" >> $CALL_SLURM_SCRIPT
+    echo -e "   step=$(( SLURM_JOB_LIMIT_RUFUS_ARG - 1 ))" >> $CALL_SLURM_SCRIPT
+    echo -e "   sbatch --array=${i}-$((i+step)) $SCAFFOLD_SLURM_SCRIPT \$i" >> $CALL_SLURM_SCRIPT
+    echo -e "done" >> $CALL_SLURM_SCRIPT
+    echo -e "sbatch --depend=afterok:$(squeue -u $USER -o '%A' -h | tr '\n' ':' | sed 's/:$//') $PP_SLURM_SCRIPT" >> $CALL_SLURM_SCRIPT
+fi	
 
 # Compose post-process slurm wrapper
 PP_SLURM_SCRIPT="rufus_post_process.slurm" # Slurm wrapper for post process script
@@ -144,23 +169,14 @@ mv rufus.cmd ${HOST_DATA_DIR_RUFUS_ARG}
 EXE_SCRIPT=launch_rufus.sh
 echo -e "#!/bin/bash" > $EXE_SCRIPT
 echo -e "" >> $EXE_SCRIPT
-echo -e "# This script should be executed after calling the container setup_slurm.sh helper. It requires $PP_SLURM_SCRIPT and all rufus_call*.sh scripts to be present in the same directory." >> $EXE_SCRIPT 
-echo -e "# Insert command for your system to load singularity here if needed (e.g. module load singularity)"
+echo -e "# This script should be executed after calling the container setup_slurm.sh helper. It requires $PP_SLURM_SCRIPT and $RUFUS_CALL_SCRIPT scripts to be present in the same directory." >> $EXE_SCRIPT 
 echo "" >> $EXE_SCRIPT
 echo -e "# Launch calling job(s)" >> $EXE_SCRIPT
-echo -e "ARRAY_JOB_ID=\$(sbatch --parsable rufus_call_1.slurm)" >> $EXE_SCRIPT
-
-i=2
-while [ "$i" -le "$NUM_CALL_SLURMS" ]; do
-	CURR_CALL_SCRIPT="rufus_call_${i}.slurm"	
-	echo -e "ARRAY_JOB_ID=\$(sbatch --parsable --depend=afterany:\$ARRAY_JOB_ID $CURR_CALL_SCRIPT)" >> $EXE_SCRIPT
-	i=$(( i + 1 ))
-done
-
+echo -e "ARRAY_JOB_ID=\$(sbatch --parsable rufus_call.slurm)" >> $EXE_SCRIPT
 echo -e "" >> $EXE_SCRIPT
 echo -e "# Launch post-process job - will wait on calling phase to complete" >> $EXE_SCRIPT
 echo -e "sbatch --depend=afterany:\$ARRAY_JOB_ID $PP_SLURM_SCRIPT" >> $EXE_SCRIPT
 
 # Give user instructions for next step
-echo -e "Slurm scripts ready to execute with $EXE_SCRIPT. Please make sure singularity is available in your environment, and then run... "
+echo -e "Slurm scripts ready to execute with $EXE_SCRIPT. Please make sure singularity is available in your environment (with e.g. module load singularity), and then run... "
 echo -e "bash $EXE_SCRIPT"
