7f2ad06aa656055397ff06388ba629a625d4aa02
stefinfection <sjeangeo@gmail.com>
2024-09-23 17:30:46 +0000

    Iterate through loop despite what index we're at

--- singularity/setup_slurm.sh
+++ singularity/setup_slurm.sh
@@ -121,17 +121,16 @@ else
     echo -e "   job_count=\$((\$job_count + 1))" >> $RUFUS_SLURM_SCRIPT
     echo -e "   num_jobs_plus_one=\$((\$SLURM_ARRAY_TASK_ID - $NUM_JOBS_BASE_COUNT))" >> $RUFUS_SLURM_SCRIPT
     echo -e "   starting_index=\$(($RUFUS_CALLS_BASE_COUNT + (\$num_jobs_plus_one * ($BASE_COUNT_PER_SCRIPT + 1))))" >> $RUFUS_SLURM_SCRIPT
-    echo -e "else" >> $RUFUS_SLURM_SCRIPT
-    echo -e "   for i in \$(seq 0 \$((\$job_count - 1))); do" >> $RUFUS_SLURM_SCRIPT
-    echo -e "       curr_job=\$((\$starting_index + \$i))" >> $RUFUS_SLURM_SCRIPT
-    echo -e "       region_arg=\$(singularity exec ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/singularity/launch_utilities/get_region.sh \"\$curr_job\" \"$WINDOW_SIZE_RUFUS_ARG\" \"$GENOME_BUILD_RUFUS_ARG\")" >> $RUFUS_SLURM_SCRIPT
-    echo -e "       REGION_ARG=\"-R \$region_arg\"" >> $RUFUS_SLURM_SCRIPT
-    echo -en "      srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> $RUFUS_SLURM_SCRIPT
+    echo -e "fi" >> $RUFUS_SLURM_SCRIPT
+    echo -e "for i in \$(seq 0 \$((\$job_count - 1))); do" >> $RUFUS_SLURM_SCRIPT
+    echo -e "    curr_job=\$((\$starting_index + \$i))" >> $RUFUS_SLURM_SCRIPT
+    echo -e "    region_arg=\$(singularity exec ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/singularity/launch_utilities/get_region.sh \"\$curr_job\" \"$WINDOW_SIZE_RUFUS_ARG\" \"$GENOME_BUILD_RUFUS_ARG\")" >> $RUFUS_SLURM_SCRIPT
+    echo -e "    REGION_ARG=\"-R \$region_arg\"" >> $RUFUS_SLURM_SCRIPT
+    echo -en "   srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> $RUFUS_SLURM_SCRIPT
     echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> rufus.cmd
 	  echo -en "-pa \$SLURM_ARRAY_TASK_ID " >> $RUFUS_SLURM_SCRIPT
     write_out_rest_of_rufus_args
-    echo -e "   done" >> $RUFUS_SLURM_SCRIPT
-    echo -e "fi" >> $RUFUS_SLURM_SCRIPT
+    echo -e "done" >> $RUFUS_SLURM_SCRIPT
   else
       # Write out the slurm header
       ADJ_CHUNK_END=$((NUM_CHUNKS - 1))
