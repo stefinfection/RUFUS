cee12c6bb495e8fb6d466b74aa510613b7b6978d
stefinfection <sjeangeo@gmail.com>
2025-01-21 17:55:55 +0000

    Remove chr10-22 because already removed from 1 and 2 asterisk

--- post_process/post_process.sh
+++ post_process/post_process.sh
@@ -26,6 +26,7 @@ clean_up_early_intermeds() {
   local CONTROLS=("${ALL_ARGS[@]:1}")
 
   # TODO: will need to not hard code eventually to accommodate other builds/species
+  # NOTE: don't need chr10-22 because 1* and 2* will get rid of these
   chroms=(
     "chr1"
     "chr2"
@@ -36,19 +37,6 @@ clean_up_early_intermeds() {
     "chr7"
     "chr8"
     "chr9"
-    "chr10"
-    "chr11"
-    "chr12"
-    "chr13"
-    "chr14"
-    "chr15"
-    "chr16"
-    "chr17"
-    "chr18"
-    "chr19"
-    "chr20"
-    "chr21"
-    "chr22"
     "chrX"
     "chrY"
   )
