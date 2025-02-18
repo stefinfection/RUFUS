aec0ea30d70bda6647d8fa9c73119b585b37ebd5
stefinfection <sjeangeo@gmail.co>
2025-02-18 12:23:28 -0700

    Output jellyfish line to sterr

--- scripts/RunJellyForRUFUS.sh
+++ scripts/RunJellyForRUFUS.sh
@@ -34,8 +34,8 @@ else
 	fi
 	mkfifo $GEN.fq
 	bash $GEN | $RDIR/bin/PassThroughSamCheck $GEN.Jelly.chr > $GEN.fq &
-	echo "$JELLYFISH count --disk -m $K -L $L -s "$hash_size" -t $T -o $GEN.Jhash -C $GEN.fq"
-	$JELLYFISH count --disk -m $K -L $L -s "$hash_size" -t $T -o $GEN.Jhash -C $GEN.fq
+	echo "$JELLYFISH count --disk -m $K -L $L -s $hash_size -t $T -o $GEN.Jhash -C $GEN.fq" >&2
+	$JELLYFISH count --disk -m $K -L $L -s $hash_size -t $T -o $GEN.Jhash -C $GEN.fq
 	rm $GEN.Jhash.temp
 	rm $GEN.fq
 
