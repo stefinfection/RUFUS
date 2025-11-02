598308e3686f658ddb3e089b9e1583b8298b8f7c
stefinfection <sjeangeo@gmail.com>
2025-11-02 23:14:58 +0000

    Update variable name in download script

--- resource_helpers/download_hash.sh
+++ resource_helpers/download_hash.sh
@@ -8,10 +8,10 @@ if [ ! -d "/mnt/rufus_resources" ]; then
     mkdir -p "/mnt/rufus_resources"
 fi
 
-chrom=$(echo "$FMTD_REG" | cut -d'_' -f1)
+chrom=$(echo "$FMTD_REGION" | cut -d'_' -f1)
 
-if [ "$REGION" == "wg" ]; then
+if [ "$FMTD_REGION" == "wg" ]; then
     aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash"
 else
-    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/${chrom}/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
+    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/${chrom}/${FMTD_REGION}_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/${FMTD_REGION}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
 fi
