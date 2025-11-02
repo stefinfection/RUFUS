923d3afe88a7a72a2ddbfe837ecc966076101c73
stefinfection <sjeangeo@gmail.com>
2025-11-02 23:12:58 +0000

    use aws cp instead of sync for hash files

--- resource_helpers/download_hash.sh
+++ resource_helpers/download_hash.sh
@@ -11,7 +11,7 @@ fi
 chrom=$(echo "$FMTD_REG" | cut -d'_' -f1)
 
 if [ "$REGION" == "wg" ]; then
-    aws s3 sync --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash"
+    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash"
 else
-    aws s3 sync --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/${chrom}/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
+    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/${chrom}/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_resources/${FMTD_REG}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
 fi
