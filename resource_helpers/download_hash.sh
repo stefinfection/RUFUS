57736e6a206d65a8406b3136696978a11e873aaf
stefinfection <sjeangeo@gmail.com>
2025-11-14 21:37:42 +0000

    Compartmentalize cleanup within container; more robust checking for hash downloads

--- resource_helpers/download_hash.sh
+++ resource_helpers/download_hash.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+# Downloaded hashes get put in /mnt/rufus_supplementals/downloaded_{hash_type}_hashes
+# File name depends only upon matching *wg* or *fmtd_region* in the correct type/version directory
 
 HASH_TYPE="$1"
 HASH_VERSION="$2"
-FMTD_REGION="$3"
+FMTD_REGION="$3" # Must be "wg" if not a region
 
-if [ "$FMTD_REGION" == "wg" ]; then
-    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_supplementals/${HASH_TYPE}_hashes/wg_${HASH_TYPE}_${HASH_VERSION}.Jhash"
+s3_path="s3://rufus.marth.lab/public_access_data/rufus_resources/${HASH_TYPE}_hashes/${HASH_VERSION}/"
+
+# Check that we only have one file to download first
+matches=$(aws s3 ls --no-sign-request $s3_path | grep "$FMTD_REGION" | awk '{print $NF}')
+count=$(echo "$matches" | grep -c "^")
+
+if [ -z "$matches" ]; then
+    count=0
 else
-    aws s3 cp --no-sign-request "s3://rufus.marth.lab/public_access_data/${HASH_TYPE}_hashes/${HASH_VERSION}/${FMTD_REGION}_${HASH_TYPE}_${HASH_VERSION}.Jhash" "/mnt/rufus_supplementals/${HASH_TYPE}_hashes/${FMTD_REGION}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
+    count=$(echo "$matches" | wc -l)
+fi
+
+if [ $count -eq 0 ]; then
+    echo "Error: No files found matching pattern '$FMTD_REGION'"
+    exit 1
+elif [ $count -gt 1 ]; then
+    echo "Error: Multiple files found matching pattern '$FMTD_REGION':"
+    echo "$matches"
+    exit 1
+else
+    aws s3 cp --no-sign-request "${s3_path}${matches}" "/mnt/rufus_supplementals/downloaded_${HASH_TYPE}_hashes/${FMTD_REGION}_${HASH_TYPE}_${HASH_VERSION}.Jhash"
 fi
