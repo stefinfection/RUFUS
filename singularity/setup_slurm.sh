bcd6f848a38250bd6e1e879459096c4c6eb10f0a
stefinfection <sjeangeo@gmail.co>
2025-12-18 11:32:20 -0700

    fix done keyword on newline in rufus_call slurm script

--- singularity/setup_slurm.sh
+++ singularity/setup_slurm.sh
@@ -162,7 +162,7 @@ else
     echo -en "   srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> $RUFUS_SLURM_SCRIPT
     echo -en "srun --mem=0 singularity exec --bind ${HOST_DATA_DIR_RUFUS_ARG}:/mnt ${CONTAINER_PATH_RUFUS_ARG} bash /opt/RUFUS/runRufus.sh -s /mnt/$SUBJECT_RUFUS_ARG " >> rufus.cmd
 	  echo -en "-pa \$SLURM_ARRAY_TASK_ID " >> $RUFUS_SLURM_SCRIPT
-	  echo -en "-cn \$curr_job " >> $RUFUS_SLURM_SCRIPT
+	  echo -e "-cn \$curr_job " >> $RUFUS_SLURM_SCRIPT
     write_out_rest_of_rufus_args
     echo -e "done" >> $RUFUS_SLURM_SCRIPT
   else
