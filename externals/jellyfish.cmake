d8bee080bf0cb8c27e6734a01672850c77af3d32
stefinfection <sjeangeo@gmail.com>
2026-01-24 01:43:29 +0000

    fixed old jellyfish macro syntax/autotools building error

--- externals/jellyfish.cmake
+++ externals/jellyfish.cmake
@@ -4,17 +4,22 @@ SET(JELLYFISH_PROJECT jellyfish_project CACHE INTERNAL "jellyfish project name")
 SET(JELLYFISH_DIR ${CMAKE_BINARY_DIR}/externals/jellyfish CACHE INTERNAL "jellyfish project directory")
 SET(JELLYFISH_LIB)
 
-
-
 ExternalProject_Add(${JELLYFISH_PROJECT}
-        URL  https://github.com/gmarcais/Jellyfish/releases/download/v2.2.5/jellyfish-2.2.5.tar.gz
+  URL https://github.com/gmarcais/Jellyfish/releases/download/v2.2.5/jellyfish-2.2.5.tar.gz
 
-        CONFIGURE_COMMAND ${PROJECT_SOURCE_DIR}/bin/externals/jellyfish/src/jellyfish_project/configure --prefix=${PROJECT_SOURCE_DIR}/bin/externals/jellyfish/src/jellyfish_project/
-        BUILD_IN_SOURCE 1
-        BUILD_COMMAND make
-        INSTALL_COMMAND make install
-        UPDATE_COMMAND ""
-        PREFIX ${JELLYFISH_DIR}
+  PATCH_COMMAND
+    patch -p0 < ${PROJECT_SOURCE_DIR}/src/externals/patches/jellyfish-configure-ac.patch
+
+  BUILD_IN_SOURCE 1
+
+  CONFIGURE_COMMAND
+    autoreconf -fi
+    COMMAND ./configure --prefix=${PROJECT_SOURCE_DIR}/bin/externals/jellyfish/src/jellyfish_project/
+
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install
+  UPDATE_COMMAND ""
+  PREFIX ${JELLYFISH_DIR}
 )
 
 ExternalProject_Get_Property(${JELLYFISH_PROJECT} INSTALL_DIR)
--- externals/modifiedJellyfish.cmake
+++ externals/modifiedJellyfish.cmake
@@ -5,16 +5,22 @@ SET(MODIFIED_JELLYFISH_DIR ${CMAKE_BINARY_DIR}/externals/modified_jellyfish CACH
 SET(MODIFIED_JELLYFISH_LIB)
 
 
-
 ExternalProject_Add(${MODIFIED_JELLYFISH_PROJECT}
 	URL ${PROJECT_SOURCE_DIR}/src/modifiedJellyfish.tar.gz
 
-        CONFIGURE_COMMAND ${PROJECT_SOURCE_DIR}/bin/externals/modified_jellyfish/src/modified_jellyfish_project/configure --prefix=${PROJECT_SOURCE_DIR}/bin/externals/modified_jellyfish/src/modified_jellyfish_project/
-        BUILD_IN_SOURCE 1
-        BUILD_COMMAND make
+ 	PATCH_COMMAND
+    		patch -p0 < ${PROJECT_SOURCE_DIR}/src/externals/patches/jellyfish-configure-ac.patch
+
+ 	BUILD_IN_SOURCE 1
+
+	CONFIGURE_COMMAND
+    		autoreconf -fi
+    		COMMAND ./configure --prefix=${PROJECT_SOURCE_DIR}/bin/externals/modified_jellyfish/src/modified_jellyfish_project/
+
+  	BUILD_COMMAND make
         INSTALL_COMMAND make install
-        UPDATE_COMMAND ""
-        PREFIX ${MODIFIED_JELLYFISH_DIR}
+	#UPDATE_COMMAND ""
+	#PREFIX ${MODIFIED_JELLYFISH_DIR}
 )
 
 ExternalProject_Get_Property(${MODIFIED_JELLYFISH_PROJECT} INSTALL_DIR)
